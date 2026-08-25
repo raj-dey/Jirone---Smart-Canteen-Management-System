0313af616ea3409551ddabcc25710460a45bc578
raj dey <rajdey@rajs-MacBook-Air.local>
2026-08-26 02:06:00 +0530

    security: ignore firebase-config.js, add firebase-config.example.js template

---
+++ firebase-config.example.js
@@ -0,0 +1,20 @@
+// firebase-config.example
+// Copy this file to firebase-config.js and populate your Firebase credentials.
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+// KEYS
+const config = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+const appInstance = initializeApp(config);
+export const db = getFirestore(appInstance);
+export const auth = getAuth(appInstance);
